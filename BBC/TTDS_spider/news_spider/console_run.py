65140d3fdce7bff5055d52ac20b00a29959b9bdd
FANWEI MENG <44691538+MengFW9@users.noreply.github.com>
2022-03-07 19:22:58 +0000

    Update console_run.py

--- BBC/TTDS_spider/news_spider/console_run.py
+++ BBC/TTDS_spider/news_spider/console_run.py
@@ -1,15 +1,21 @@
 from scrapy import cmdline
 import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+import time
 
 name = 'BBC_news'
 cmd1 = 'scrapy crawl {0} '.format(name)
 def crawler_command():
+    time_now = time.time()
+    global cmd1
+    cmd1 = cmd1 + "-s LOG_FILE={0}.log".format(time_now)
     cmdline.execute(cmd1.split())
 
 
 if __name__ == '__main__':
-    scheduler = BlockingScheduler(timezone="Europe/London")
-    scheduler.add_job(crawler_command, 'interval', hours=12, id='test_job1',next_run_time=datetime.datetime.now())
-    scheduler.start()
-
+    #scheduler = BlockingScheduler(timezone="Europe/London")
+    #scheduler.add_job(crawler_command, 'interval', hours=12, id='test_job1',next_run_time=datetime.datetime.now())
+    #scheduler.start()
+    while True:
+        crawler_command()
+        time.sleep(43200)
