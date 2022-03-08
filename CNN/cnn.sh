e33724ba833b45290d20ade7b798e97683459a40
Zheng Ying <42151586+caohongchuan@users.noreply.github.com>
2022-03-08 14:23:42 +0000

    Update cnn.sh

--- CNN/cnn.sh
+++ CNN/cnn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # conda remove --name spider --all
-# conda env create -f environment.yml
+# conda env create -f environment.ymlhttps://github.com/ttdsgroup7/crawlnews/blob/main/CNN/cnn.sh
 # sleep(15)
-conda activate spider
-sleep(5)
+# conda activate spider
+# sleep 5
 python CNN_scrapy.py
